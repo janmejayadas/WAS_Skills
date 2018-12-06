00fd0b607dd4ae704335fcd23544dad29cb935a5
janmejayadas <43834059+janmejayadas@users.noreply.github.com>
2018-12-06 14:43:40 +0530

    Use less

--- run-server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-export CONVERSATION_USERNAME=$(echo "$service_watson_conversation" | jq -r '.username')
-export CONVERSATION_PASSWORD=$(echo "$service_watson_conversation" | jq -r '.password')
-npm start
