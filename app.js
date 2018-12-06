825606df2a5c13e5bfe909451581706f0645844d
janmejayadas <43834059+janmejayadas@users.noreply.github.com>
2018-12-06 14:39:45 +0530

    Make Entire Code Replace

--- app.js
+++ app.js
@@ -16,15 +16,12 @@
 
 'use strict';
 
-var WatsonConversationSetup = require('./lib/watson-conversation-setup');
-var DEFAULT_NAME = 'watson-conversation-slots-intro';
-var fs = require('fs'); // file system for loading JSON
-var vcapServices = require('vcap_services');
-var conversationCredentials = vcapServices.getCredentials('conversation');
-var watson = require('watson-developer-cloud'); // watson sdk
-
 var express = require('express'); // app server
 var bodyParser = require('body-parser'); // parser for post requests
+var {converse} = require('./sagan');
+const AuthorizationV1 = require('watson-developer-cloud/authorization/v1');
+const SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+const TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
 
 var app = express();
 
@@ -32,80 +29,84 @@ var app = express();
 app.use(express.static('./public')); // load UI from public folder
 app.use(bodyParser.json());
 
-var workspaceID; // workspaceID will be set when the workspace is created or validated.
-
-const conversation = new watson.AssistantV1({ 
-  version: '2018-02-16'
-});
-
-var conversationSetup = new WatsonConversationSetup(conversation);
-var workspaceJson = JSON.parse(fs.readFileSync('data/watson-pizzeria.json'));
-var conversationSetupParams = { default_name: DEFAULT_NAME, workspace_json: workspaceJson };
-conversationSetup.setupConversationWorkspace(conversationSetupParams, (err, data) => {
-  if (err) {
-    //handleSetupError(err);
-  } else {
-    console.log('Assistant is ready!');
-    workspaceID = data;
-  }
-});
-
 // Endpoint to be call from the client side
 app.post('/api/message', function(req, res) {
-
-  if (!workspaceID) {
+  if (req && req.body && req.body.input && req.body.input.text && req.body.input.text !== '') {
+    converse(req.body.input, (err, result) => {
+      if (err) {
+        return res.status(err.code || 500).json(err);
+      }
+      //checkResultForLocationInfo(result);
+      return res.json(updateMessage(result));
+    });
+  } else {
     return res.json({
-      output: {
-        text: 'Assistant initialization in progress. Please try again.'
+      speech: {
+        text: `Hi, what can I do for you?`
       }
     });
   }
+});
 
-  var payload = {
-    workspace_id: workspaceID,
-    context: req.body.context || {},
-    input: req.body.input || {}
-  };
+const SpeechToText = new SpeechToTextV1({
+  username: process.env.WATSON_SPEECH_TO_TEXT_USERNAME,
+  password: process.env.WATSON_SPEECH_TO_TEXT_PASSWORD,
+});
 
-  // Send the input to the conversation service
-  conversation.message(payload, function(err, data) {
-    if (err) {
-      return res.status(err.code || 500).json(err);
+//const authService = new AuthorizationV1(SpeechToText.getCredentials());
+const sttAuthService = new AuthorizationV1(SpeechToText.getCredentials());
+
+app.use(`/api/speech-to-text/token`, (req, res) => {
+  //authService.getToken((error, token) => {
+  sttAuthService.getToken((error, token) => {
+       if (error) {
+         console.log('Error:', error);
+       } else {
+         res.send(token);
+       }
+     });
+   });
+
+   const TextToSpeech = new TextToSpeechV1({
+     username: process.env.WATSON_TEXT_TO_SPEECH_USERNAME,
+     password: process.env.WATSON_TEXT_TO_SPEECH_PASSWORD,
+   });
+
+   const ttsAuthService = new AuthorizationV1(TextToSpeech.getCredentials());
+
+   app.use(`/api/text-to-speech/token`, (req, res) => {
+     ttsAuthService.getToken((error, token) => {
+    if (error) {
+      console.log('Error:', error);
+    } else {
+      res.send(token);
     }
-    return res.json(updateMessage(payload, data));
   });
 });
 
+
 /**
  * Updates the response text using the intent confidence
- * @param  {Object} input The request to the Assistant service
- * @param  {Object} response The response from the Assistant service
+ * @param  {Object} response The response from the Conversation service
  * @return {Object}          The response with the updated message
  */
-function updateMessage(input, response) {
-  var responseText = null;
-  if (!response.output) {
-    response.output = {};
-  } else {
-    return response;
-  }
-  if (response.intents && response.intents[0]) {
-    var intent = response.intents[0];
-    // Depending on the confidence of the response the app can return different messages.
-    // The confidence will vary depending on how well the system is trained. The service will always try to assign
-    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
-    // user's intent . In these cases it is usually best to return a disambiguation message
-    // ('I did not understand your intent, please rephrase your question', etc..)
-    if (intent.confidence >= 0.75) {
-      responseText = 'I understood your intent was ' + intent.intent;
-    } else if (intent.confidence >= 0.5) {
-      responseText = 'I think your intent was ' + intent.intent;
-    } else {
-      responseText = 'I did not understand your intent';
+function updateMessage(response) {
+  //response.output = {text: response.speech.text};
+  return response;
+}
+
+function checkResultForLocationInfo(result) {
+  if (result.expertise.name) {
+    if (result.expertise.name === "restaurant") {
+      var card = result.card
+      if (card) {
+        if (card.content[0]) {
+          process.env.lat = card.content[0].latitude
+          process.env.long = card.content[0].longitude
+        }
+      }
     }
   }
-  response.output.text = responseText;
-  return response;
 }
 
 module.exports = app;
